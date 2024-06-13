6e3ad8ba37030104b9dd930ab980bd1173910d15
Logan Cole <emtlogancole@gmail.com>
2024-06-13 12:40:16 -0500

    show output shape

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ raw_inputs = [
 ]
 inputs = tokenizer(raw_inputs, padding=True, truncation=True, return_tensors="pt")
 print(inputs)
+
+from transformers import AutoModelForSequenceClassification
+
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+raw_inputs = [
+    "I've been waiting for a HuggingFace course my whole life.",
+    "I hate this so much!",
+]
+outputs = model(**inputs)
+print(outputs.logits.shape)
